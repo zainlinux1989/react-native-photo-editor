4da2528dd2c1422ab84074a1f87043ef181ebd60
atey70 <atey70@gmail.com>
2025-05-10 11:27:18 +0200

    ff 030

--- android/src/main/java/com/reactnativephotoeditor/PhotoEditorModule.kt
+++ android/src/main/java/com/reactnativephotoeditor/PhotoEditorModule.kt
@@ -79,13 +79,13 @@ class PhotoEditorModule(reactContext: ReactApplicationContext) : ReactContextBas
         }
       }
     if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-        val result = ActivityResult(data)
-        if (resultCode == RESULT_OK) {
-            val resultUri = result?.uriContent
-            // використовуй результат
-        } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-            val error = result?.error
-        }
+        // val result = ActivityResult(data)
+        // if (resultCode == RESULT_OK) {
+        //     val resultUri = result?.uriContent
+        //     // використовуй результат
+        // } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+        //     val error = result?.error
+        // }
       }
     }
   }
